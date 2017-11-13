32589d9d13df33bee4471f7fed2aafe4e5035b7f
BB20101997 <bb-github@t-online.de>
2017-11-13 23:06:13 +0100

    Forgot to add the File content in the last commit

--- haskell/blat4/List.hs
+++ haskell/blat4/List.hs
@@ -1,2 +1,26 @@
+#!/usr/bin/ghci
 module List where
 
+reversedRecursiv::[a]->[a]
+reversedRecursiv [] = []
+reversedRecursiv (a:b) = (reversedRecursiv b)++[a]
+
+reversed::[a]->[a]
+reversed a = revHelp a []
+    where
+        revHelp::[a]->[a]->[a]
+        revHelp (h:t) b = revHelp t (h:b)
+        revHelp [] a = a
+
+indexOf::Int->[Int]->Maybe Int
+indexOf e list = indexHelp e list 0
+    where
+        indexHelp::Int->[Int]->Int->Maybe Int
+        indexHelp e (h:t) i
+            | e == h = Just i
+            | otherwise = indexHelp e t $! (i+1)
+        indexHelp _ [] _ = Nothing
+--inits
+--tails
+--insert
+--perms
