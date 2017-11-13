38d9f544fff7446eb7d51243465fe7b4e7d830aa
BB20101997 <bb-github@t-online.de>
2017-11-13 21:44:28 +0100

    fixed stupid errors

--- haskell/blat4/Tree.hs
+++ haskell/blat4/Tree.hs
@@ -2,28 +2,28 @@
 module Tree where
 
 data Tree a = Leaf a
-            | Branch Tree a Tree a
+            | Branch (Tree a) (Tree a)
 
 --rekursiv
 sumTreeRec::Tree Int -> Int
 sumTreeRec (Leaf a) = a
-sumTreeRec (Tree a b) = (sumTreeRec a) + (sumTreeRec b)
+sumTreeRec (Branch a b) = (sumTreeRec a) + (sumTreeRec b)
 
---mit Akkumulator? TODO ist das so richtig?
+--mit Akkumulator
 sumTreeAku::Tree Int ->Int
 sumTreeAku b = akuHelper b 0
     where
-        akuHelper::Tree->Int->Int
-        akuHelper (Leaf a) aku = aku+a+b
+        akuHelper::Tree Int->Int->Int
+        akuHelper (Leaf a) aku = aku+a
         akuHelper (Branch a b) aku = akuHelper a $! (akuHelper b aku)
 
-mirrorTree::Tree->Tree
+mirrorTree::Tree a->Tree a
 mirrorTree (Leaf a) = (Leaf a)
 mirrorTree (Branch a b) = (Branch (mirrorTree b) (mirrorTree a))
 
 treeToList::Tree a->[a]
 treeToList t = ttlHelp t []
     where
-        ttlHel::Tree a->[a]->[a]
+        ttlHelp::Tree a->[a]->[a]
         ttlHelp (Leaf a) list = a : list
-        ttlHelp (Tree a b) list = (ttlHelp a $! (ttlHelp b list))
+        ttlHelp (Branch a b) list = (ttlHelp a $! (ttlHelp b list))
