9c7f1ac9f7e94fdfbfdf366bc5bd8ef5af187113
BB20101997 <bb-github@t-online.de>
2017-11-14 23:18:29 +0100

    added shebang fixed missing i's in line 7 and 8

--- haskell/blat4/BinaryTree.hs
+++ haskell/blat4/BinaryTree.hs
@@ -1,10 +1,11 @@
+#!/usr/bin/ghci
 data SearchTree = Branch SearchTree SearchTree Int
                 | Empty
 
 insert::SearchTree->Int->SearchTree
 insert (Branch l r i) n
-        | i>n =  Branch (insert l n) r
-        | i<=n =  Branch l (insert r n)
+        | i>n =  Branch (insert l n) r i
+        | i<=n =  Branch l (insert r n) i
 insert Empty n = Branch Empty Empty n
 
 isElem::SearchTree->Int->Bool
