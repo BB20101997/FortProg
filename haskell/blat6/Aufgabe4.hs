691875591aa25b7d3d4ea192a957c00922f8b89e
Sloner42 <sloner@web.de>
2017-11-28 12:48:08 +0100

    Hier

--- haskell/blat6/Aufgabe4.hs
+++ haskell/blat6/Aufgabe4.hs
@@ -4,11 +4,23 @@ erstens:: (a->c->d)->(b->c)->b->a->c
 erstens f g x y = f (g x) y
 -}
 erstens:: a->b->c
-erstens x y = f (g x) y
+erstens = f . g
 
 --1.2
 zweitens:: a->b->c
-zweitens x y = f (g (h x y)))
+zweitens=f . (.) . (.) g h
 --1.3
 drittens:: (a->b)->(b-c)->a->c
-drittens f g x = g (f x)
+drittens f g = g . f
+
+
+--2.1
+flipid:: b->(b->c)->c
+flipid = \x f-> f x
+--2.2
+eule::(b->c)->(a1->a2-b)->a1->a2->c
+eule = \f g x y -> f . g x y
+
+--3.
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipwith f x y = map f (zip x y)
