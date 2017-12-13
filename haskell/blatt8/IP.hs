f683f8077c72e576b4cb02678f49c6f9440df7b2
BB20101997 <bennet-blessmann@t-online.de>
2017-12-13 13:46:14 +0100

    added comments fixed spelling in comments fixed Einrückung made menuWithOnSelect use itself instead of menu in rekursion

--- haskell/blatt8/IP.hs
+++ haskell/blatt8/IP.hs
@@ -6,9 +6,10 @@ printMenu = print 1
         print i (x:[]) = putStrLn ((show i)++": "++x)
         print i (x:xs) = do
                             putStrLn (show i++": "++x++",") -- new line after entry looks better than just a space (personal preference)
+                            --putStr (show i++": "++x++", ") -- the way it would be with a space instead of a line break after each entry
                             print (i+1) xs
 
---try getting input till it is a number betrween 1 and to (both inclusive)
+--try getting input between 1 and to (both inclusive) if so return that value else -1
 select::Int->IO Int
 select to = do 
                 x <- getLine
@@ -19,6 +20,8 @@ select to = do
                             ((i,_):_) | i>0 && i<=to -> return i
                             _ -> return (-1)
 
+
+--3.1
 menu::[String] -> IO Int
 menu [] =  return (0-1)
 menu entryList = do
@@ -31,6 +34,7 @@ menu entryList = do
                                     menu entryList                                    
                         _  -> return i
 
+--3.2
 wordCount::FilePath->IO()
 wordCount file = do
                     x<-menu ["count chars","count words","count lines","quit"]
@@ -51,6 +55,7 @@ wordCount file = do
                         _ -> do 
                                 putStrLn "Bye"
 
+--3.3
 onSelect::[(String,IO a)]->IO a
 onSelect actions = do
                         printMenu [x|(x,_)<-actions]
@@ -66,26 +71,27 @@ onSelect actions = do
 
 wordCountWithOnSelect::FilePath->IO()
 wordCountWithOnSelect file =    onSelect [
-                                ("count chars",do
-                                                    content <- readFile file
-                                                    putStr (show (length content))
-                                                    putStrLn " Characters"
-                                                    wordCount file
-                                ),
-                                ("count words",do 
-                                                    content <- readFile file
-                                                    putStr (show (length (words content)))
-                                                    putStrLn " Words"                                               
-                                                    wordCount file
-                                ),
-                                ("count lines",do 
-                                                    content <- readFile file
-                                                    putStr (show (length (lines content)))
-                                                    putStrLn " Lines"
-                                                    wordCount file
-                                ),
-                                ("quit" ,putStrLn "Bye")
-                             ]
+                                             ("count chars",do
+                                                                content <- readFile file
+                                                                putStr (show (length content))
+                                                                putStrLn " Characters"
+                                                                wordCountWithOnSelect file
+                                            ),
+                                            ("count words",do 
+                                                                content <- readFile file
+                                                                putStr (show (length (words content)))
+                                                                putStrLn " Words"                                               
+                                                                wordCountWithOnSelect file
+                                            ),
+                                            ("count lines",do 
+                                                                content <- readFile file
+                                                                putStr (show (length (lines content)))
+                                                                putStrLn " Lines"
+                                                                wordCountWithOnSelect file
+                                            ),
+                                            ("quit" ,putStrLn "Bye")
+                                         ]
 
+--3.4
 menuWithOnSelect::[String]->IO Int
 menuWithOnSelect entries = onSelect [(x,return a)|(x,a)<- (zip entries [1..])] 
