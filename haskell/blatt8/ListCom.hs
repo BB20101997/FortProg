5e2532c2dcef99651b9f9e04325da08c4b7f8858
Sloner42 <sloner@web.de>
2017-12-13 12:40:45 +0100

    Finished

---
+++ haskell/blatt8/ListCom.hs
@@ -0,0 +1,29 @@
+import Data.Maybe
+
+first::[Int]
+first = [(2*i)+1 | i<-[1..5]]
+
+second::[(Int,Bool)]
+second = [(5*i,(mod i 2 == 0)) | i<-[1..5], i/=2,i/=3]
+
+third::[Maybe Int]
+third = [Just (i^2) | i<-[1..5], mod i 2==1   ]
+
+
+fourth::[(Int,Int)]
+fourth = [(i,j) | i<-[1..5], j<-[5,4..1], j>i]
+
+mapWithComp::(a->b)->[a]->[b]
+mapWithComp f a = [f c |c<-a ]
+
+lookupWithComp:: Eq a => a -> [(a, b)] -> Maybe b
+lookupWithComp elem list = listToMaybe [e | (d,e)<-list, d==elem]
+
+replicateWithComp:: Int -> a -> [a] 
+replicateWithComp n obj = take n [obj]
+
+filterWithComp::(a -> Bool) -> [a] -> [a]
+filterWithComp f list = [c | c<-list, f c]
+
+test::[(Int,Int)]
+test = [(1,5),(2,6),(3,7),(4,8),(5,9),(6,0)]
