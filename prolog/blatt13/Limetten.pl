c7f14d9f27b4d72b7fe6011ce97cca9c9e125842
Sloner42 <sloner@web.de>
2018-01-31 12:12:46 +0100

    Nun mit Kommentar zur annahme

--- prolog/blatt13/Limetten.pl
+++ prolog/blatt13/Limetten.pl
@@ -4,16 +4,14 @@ limettes(K, M) :-
             K = [99,L], %Preis vom gekauften
 			L in 1..2000, % 1 Cent bis 20 Euro (nicht gratis)
 			M = [A,E], %werte des Kassenbongs anfang und ende
-			A in 1..9, %Da mit Zwanziger bezahlt min 1
-            E in 0..9,
-			99+16*L#= 1000*A+200+E, % Zuckerrohr und 16 Limetten sind A2,0E
+			A in 0..9,
+			E in 0..9,
+			99+16*L#= 1000*A+200+E, %(Annahme: Gesamtpreis für alle einheiten Rohrzucker ist 99cent und nicht Einheitenpreis) Rohrzucker und 16 Limetten sind zusammen A2,0E
 			labeling([],K).
 			
 			/*
-			L = 69, Limetten kosten 69 cent
+L = 69, Limetten kosten 69 cent
 M = [1, 3] 
-Unknown action: , (h for help)
-Action? ;
 L = 194,   Limetten kosten 1,94 Euro
 M = [3, 3] ;
 L = 319,   Limetten kosten 3,19 Euro
@@ -21,4 +19,4 @@ M = [5, 3] ;
 L = 444,    Limetten kosten 4,44 Euro
 M = [7, 3] ;
 L = 569,    Limetten kosten 5,69 Euro
-M = [9, 3].
+M = [9, 3]. */
